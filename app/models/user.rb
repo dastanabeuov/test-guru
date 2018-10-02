63a7f3be779dc2ba97564cf9a2491fcf5f329f2f
dastanabeuov <d.abeuov9@gmail.com>
2018-10-02 20:48:16 +0600

    renew edit by 02.09.2018 lesson4 finish version

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   def info_passing_test(level)
     Test.joins('join results on tests.id = results.test_id')
-    .where("results.user_id = #{ self.id } and tests.level = #{ level }")
+    .where("results.user_id = tests.user_id and tests.level = level ")
   end
 
 
