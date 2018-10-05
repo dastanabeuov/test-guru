0fbc7c06ba5153f1a334c6a1fd2e1eab30753bed
dastanabeuov <d.abeuov9@gmail.com>
2018-10-05 20:04:33 +0600

    add diagram and model add assocation method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
+  has_many :results
+  has_many :users, through: :results
+
+  has_many :questions
 
   def self.titles_by_category(category)
     Test.joins('join categories on categories.id = tests.category_id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :results
+  has_many :tests, through: :results
+
   def info_passing_test(level)
     Test.joins("join results on tests.id = results.test_id")
     .where("results.user_id = #{ self.id } and tests.level = #{ level }")
