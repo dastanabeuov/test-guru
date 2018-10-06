2693b43724c50376f39f0e82b034b12431357ad4
dastanabeuov <d.abeuov9@gmail.com>
2018-10-06 17:51:25 +0600

    edit by 06.09.2018 lesson 5

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,14 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :user
 
   has_many :results
   has_many :users, through: :results
 
-  has_many :questions
+  has_many :questions, dependent: :destroy
 
   def self.titles_by_category(category)
-    Test.joins('join categories on categories.id = tests.category_id')
+    Test.joins(:category)
     .where(categories: { title: category })
     .order(title: :desc).pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_many :results
+
   has_many :tests, through: :results
 
   def info_passing_test(level)
-    Test.joins("join results on tests.id = results.test_id")
+    Test.joins(:results)
     .where("results.user_id = #{ self.id } and tests.level = #{ level }")
   end
 end
